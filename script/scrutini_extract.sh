da5fd4691c435d9261ef6849124da8f5393a13bf
aborruso <aborruso@gmail.com>
2024-10-28 17:32:56 +0100

    update

--- script/scrutini_extract.sh
+++ script/scrutini_extract.sh
@@ -23,4 +23,22 @@ flatterer --force "${folder}"/tmp/scrutini_preferenze_comune.json "${folder}"/tm
 
 cp -r "${folder}"/tmp/scrutini_preferenze/ "${folder}"/../dati/comune
 
+# scrutini
 
+## principale
+
+mlr --icsv --ojsonl --from "${folder}"/../dati/comune/scrutini_risultati/csv/main.csv put '$minint_elettorale="107".fmtnum($int_cod_prov,"%03d").fmtnum($int_cod_com,"%04d")' >"${folder}"/tmp/scrutini_risultati/main.jsonl
+
+mlr --jsonl cut -f minint_elettorale,com_istat_code /"${folder}"/../riferimenti/comuni_07.jsonl >"${folder}"/tmp/scrutini_risultati/comuni.jsonl
+
+mlr --jsonl join --ul -j minint_elettorale -f "${folder}"/tmp/scrutini_risultati/main.jsonl then unsparsify /"${folder}"/tmp/scrutini_risultati/comuni.jsonl >"${folder}"/tmp/scrutini_risultati/main_comuni.jsonl
+
+mlr --ijsonl --ocsv --from "${folder}"/tmp/scrutini_risultati/main_comuni.jsonl put '$int_perc_vot=sub(string($int_perc_vot),",",".")' >"${folder}"/../dati/scrutini_comuni_principale.csv
+
+## candidati
+
+mlr --csv --from "${folder}"/../dati/scrutini_comuni_principale.csv cut -f _link,com_istat_code then label _link_main >"${folder}"/tmp/scrutini_risultati/scrutini_comuni_principale_ref.csv
+
+mlr --csv join --ul -j _link_main -f "${folder}"/../dati/comune/scrutini_risultati/csv/cand.csv then unsparsify "${folder}"/tmp/scrutini_risultati/scrutini_comuni_principale_ref.csv >"${folder}"/../dati/scrutini_comuni_candidati.csv
+
+mlr -I --csv  put '$perc=sub(string($perc),",",".");$perc_lis=sub(string($perc_lis),",",".")' "${folder}"/../dati/scrutini_comuni_candidati.csv
