f6b111e5616a043993f542449f077ad874f25528
aborruso <aborruso@gmail.com>
2024-10-31 12:23:39 +0100

    pulizia

--- script/affluenza_download.sh
+++ script/affluenza_download.sh
@@ -29,7 +29,20 @@ PROVINCE=$(jq -r '.enti.enti_f[] | select(.tipo=="PROVINCIA") | .cod' "${folder}
 
 # 2. Scarica i dati a livello provinciale
 for COD_PROV in $PROVINCE; do
-  curl "https://eleapi.interno.gov.it/siel/PX/votanti/DE/20241027/TE/07/PR/$COD_PROV" -H 'accept: application/json, text/plain, */*' -H 'accept-language: it,en-US;q=0.9,en;q=0.8' -H 'origin: https://elezioni.interno.gov.it' -H 'priority: u=1, i' -H 'referer: https://elezioni.interno.gov.it/' -H 'sec-ch-ua: "Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"' -H 'sec-ch-ua-mobile: ?0' -H 'sec-ch-ua-platform: "Windows"' -H 'sec-fetch-dest: empty' -H 'sec-fetch-mode: cors' -H 'sec-fetch-site: same-site' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' >"${folder}"/../dati/provincia/affluenza_provincia_$COD_PROV.json
+  curl "https://eleapi.interno.gov.it/siel/PX/votanti/DE/20241027/TE/07/PR/$COD_PROV" \
+    -H 'accept: application/json, text/plain, */*' \
+    -H 'accept-language: it,en-US;q=0.9,en;q=0.8' \
+    -H 'origin: https://elezioni.interno.gov.it' \
+    -H 'priority: u=1, i' \
+    -H 'referer: https://elezioni.interno.gov.it/' \
+    -H 'sec-ch-ua: "Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"' \
+    -H 'sec-ch-ua-mobile: ?0' \
+    -H 'sec-ch-ua-platform: "Windows"' \
+    -H 'sec-fetch-dest: empty' \
+    -H 'sec-fetch-mode: cors' \
+    -H 'sec-fetch-site: same-site' \
+    -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
+    >"${folder}"/../dati/provincia/affluenza_provincia_$COD_PROV.json
 
   # Estrai i codici dei comuni dal file provinciale
   COMUNI=$(jq -r '.enti.enti_f[] | select(.tipo=="COMUNE") | .cod' "${folder}"/../dati/provincia/affluenza_provincia_$COD_PROV.json)
